9884f8dbba5e29b21ebb226940dec19698ce9bf2
Minho Lee <minho.lee.93@hotmail.com>
2018-03-20 12:13:47 -0400

    adding basic structure, using antd, node-sass-chokidar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { 
+	Layout,
+	Menu,
+	Icon
+ } from 'antd';
+
+import './scss/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		collapsed: false,
+	}
+	onCollapse = (collapsed) => {
+		console.log(collapsed);
+		this.setState({ collapsed });
+	}
+	render() {
+		const { Header, Footer, Sider, Content } = Layout;
+		const { SubMenu } = Menu;
+		return (
+			<div>
+				<Layout style={{ minHeight: '100vh' }}>
+					<Sider
+						collapsible
+						collapsed={this.state.collapsed}
+						onCollapse={this.onCollapse}
+					>
+						<div className="logo" />
+						<Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+							<Menu.Item key="1">
+								<Icon type="pie-chart" />
+								<span>Option 1</span>
+							</Menu.Item>
+							<Menu.Item key="2">
+								<Icon type="desktop" />
+								<span>Option 2</span>
+							</Menu.Item>
+							<SubMenu
+								key="sub1"
+								title={<span><Icon type="user" /><span>User</span></span>}
+							>
+								<Menu.Item key="3">Tom</Menu.Item>
+								<Menu.Item key="4">Bill</Menu.Item>
+								<Menu.Item key="5">Alex</Menu.Item>
+							</SubMenu>
+							<SubMenu
+								key="sub2"
+								title={<span><Icon type="team" /><span>Team</span></span>}
+							>
+								<Menu.Item key="6">Team 1</Menu.Item>
+								<Menu.Item key="8">Team 2</Menu.Item>
+							</SubMenu>
+							<Menu.Item key="9">
+								<Icon type="file" />
+								<span>File</span>
+							</Menu.Item>
+						</Menu>
+					</Sider>
+					<Layout>
+						<Header>Header</Header>
+						<Content>Content</Content>
+						<Footer>Footer</Footer>
+					</Layout>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/containers/QueueBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class QueueBar extends Component {
+	render() {
+		return(
+
+		);
+	}
+}
+
+export default QueueBar;
